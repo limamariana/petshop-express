84d3a24a767b8dd747ccbc9b6e7b77bbbda11e56
marianaRodLima <marianarlima93@gmail.com>
2021-04-01 15:47:55 -0300

    Desafio 01/04

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 const express = require("express");
+const petshop = require("./petshop");
+//const {uuid} = require("uuidv4");
 const app = express();
+
+app.use(express.json())
 
 app.listen(3000, () => {
 console.log("Servidor rodando");
 });
+
+//console.log(petshop.listarPets());
+
+app.get("/pets", (request, response) =>{
+    
+    return response.send(petshop.listarPets());
+})
+
+app.get("/pets/:nome", (request, response) =>{
+    const {nome} = request.params;
+
+    const petIndex = petshop.buscarPet(nome);
+ 
+    if(!petIndex) {
+    return response.status(400).send({error: "Pet não encontrado"})
+
+}
+    return response.status(200).send(petIndex);
+});
+app.post("/pets", (request, response) => {
+    const {nome, tipo, idade, raca, peso, tutor, contato, vacinado, servicos} = request.body;
+
+    const pet = {nome, tipo, idade, raca, peso, tutor, contato, vacinado, servicos};
+
+   
+    petshop.adicionarPet(pet);
+    return response.json(pet);
+})
